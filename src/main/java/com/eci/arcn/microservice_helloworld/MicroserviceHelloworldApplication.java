1030599d61bc8175bbfb3a417d03f4d1ca516887
idsanchezf <156478003+idsanchezf@users.noreply.github.com>
2025-03-27 22:39:42 +0000

    Agrego servicio hello world

--- src/main/java/com/eci/arcn/microservice_helloworld/MicroserviceHelloworldApplication.java
+++ src/main/java/com/eci/arcn/microservice_helloworld/MicroserviceHelloworldApplication.java
@@ -2,9 +2,18 @@ package com.eci.arcn.microservice_helloworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.*;
 
+@Controller
 @SpringBootApplication
 public class MicroserviceHelloworldApplication {
+
+	@RequestMapping("/")
+    @ResponseBody
+    String home() {
+      return "Hello World!";
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(MicroserviceHelloworldApplication.class, args);
