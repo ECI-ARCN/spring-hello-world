af20cc0013531c404a947ce3a1f62bc60e2cf62a
idsanchezf <156478003+idsanchezf@users.noreply.github.com>
2025-03-27 04:36:24 +0000

    Quedo melo el container

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 
+# Actualizar paquetes
+apt-get update
+
+# Instalar Node.js (requerido por Heroku CLI)
+if ! node -v &>/dev/null; then
+    echo "Instalando Node.js..."
+    apt-get install -y nodejs npm
+else
+    echo "Node.js ya está instalado."
+fi
+
 # Instalar Heroku CLI
 curl https://cli-assets.heroku.com/install.sh | sh
 
 # Instalar Maven
-apt-get update && apt-get install -y maven
-
-# Instalar Gradle
-apt-get install -y gradle
+if ! mvn -version &>/dev/null; then
+    echo "Instalando Maven..."
+    apt-get update && apt-get install -y maven
+else
+    echo "Maven ya está instalado."
+fi
 
 # Instalar otras dependencias necesarias
 apt-get install -y jq unzip
